9ce57989607d3d2d3d7a3af829bd5e58963671ff
Kacper Włodarczyk <kacper.wlodarczyk80@gmail.com>
2017-02-21 21:57:36 +0100

    init

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: ["./", "./src"]
+        }
+    })
+
+    gulp.watch(['src/*.html', 'src/*.js']).on('change', browserSync.reload);
+})
+
+gulp.task('default', ['serve'])
+
