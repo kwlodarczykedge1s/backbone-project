199705a20bade291b249e2a98f0cc085291401ed
Kacper Włodarczyk <kacper.wlodarczyk80@gmail.com>
2017-02-25 20:25:34 +0100

    commit

--- src/jquery-example.js
+++ src/jquery-example.js
@@ -1,7 +1,7 @@
 $(function () {
     selectImportantCells()
     const uri = 'http://localhost:9001/'
-    var dataObject = {}
+    var dataObject = {};
     var tBody = $('#tBody');
     var $addPerson = $('<button>', {
         click: function () {
@@ -18,31 +18,41 @@ $(function () {
                     console.log('dane wysłane')
                 }).catch(function () {
                     console.log('błąd')
-                })
+                });
 
         },
         text: 'Add Person',
         class: 'btn btn-primary'
+    });
+
+    var $howElements = $('<button>', {
+        text: 'Ile elementów listy?',
+        class: 'btn btn-primary',
+        click: function () {
+            var listElems = $('tr').length - 1
+            alert('Dodanych ludzi: ' + listElems)
+        }
+    });
+
+    var $searchInput = '<input type="text" id="lastNameSearch">';
+
+    var $deleteUser = '<button id="deleteUser">x</button>';
+    $($deleteUser).on('click', '#tr', function () {
     })
 
-    var $deleteUser = '<button id="deleteUser">x</button>'
-    $($deleteUser).on('click', 'th', function () {
-        console.log('asdas')
-    })
-
-    $('#addPerson').append($addPerson)
+    $('#addPerson').append([$addPerson, $howElements, $searchInput])
 
     $.get(uri + 'users', function (resp) {
         const data = resp;
         $.each(data, function (key, value) {
             $(tBody).append(
-                '<tr><td>' + value.index + '</td><td>' + value.name + '</td> <td>' + value.gender + '</td><td>' + $deleteUser + '</td></tr>')
-                .css('list-style-type', 'none')
+                '<tr id="tr"><td>' + value.index + '</td><td>' + value.name + '</td> <td>' + value.gender + '</td><td>' + $deleteUser + '</td></tr>')
+                .css('list-style-type', 'none');
         })
         $('tbody').on('click', '#deleteUser', function (event) {
-            var elementOfTable = $('tbody #deleteUser').index(this)
-            var clickedObject = data[elementOfTable]
-            var id = clickedObject._id
+            var elementOfTable = $('tbody #deleteUser').index(this);
+            var clickedObject = data[elementOfTable];
+            var id = clickedObject._id;
 
             $.ajax({
                 url: uri + 'users?_id=' + id,
@@ -55,19 +65,21 @@ $(function () {
                     console.log('success')
                 })
                 .catch(function (err) {
-                    console.log('error: ' + err)
-                })
-        })
+                    $.each(err, function () {
+                        console.log(err.statusText)
+                    });
+                });
+        });
     })
 
     function selectImportantCells() {
-        $('tbody').on('click', 'td', function () {
+        $('tbody').on('click', 'tr', function () {
             $(this).css({
                 'background-color': 'red',
                 'font-weight': 'bold',
                 'color': 'white',
                 'font-size': '16px;'
-            })
-        })
+            });
+        });
     }
-})
+});
