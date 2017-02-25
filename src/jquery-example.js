4c02377358c354fc69ce7f1c24055cfdb9afe09e
Kacper Włodarczyk <kacper.wlodarczyk80@gmail.com>
2017-02-25 19:05:56 +0100

    ajax examples

--- src/jquery-example.js
+++ src/jquery-example.js
@@ -1,42 +1,62 @@
-$(function(){
-        const uri = 'http://localhost:9001/'
-        var dataObject = {}
-        var tBody = $('#tBody');
-        var $addPerson = $('<button>', {
-            click: function(){
-               const personData = {
-                   id: null,
-                   name: 'John Doe',
-                   gender: 'Male'
-               }
-            
-               $.post(uri + 'users', personData, function(data){
-                   console.log(data)
-               })
-                .then(function(){
+$(function () {
+    const uri = 'http://localhost:9001/'
+    var dataObject = {}
+    var tBody = $('#tBody');
+    var $addPerson = $('<button>', {
+        click: function () {
+            const personData = {
+                _id: null,
+                name: 'John Doe',
+                gender: 'Male'
+            }
+
+            $.post(uri + 'users/', personData, function (data) {
+                console.log(data)
+            })
+                .then(function () {
                     console.log('dane wysłane')
-                }).catch(function(){
+                }).catch(function () {
                     console.log('błąd')
                 })
 
-            },
-            text: 'Add Person',
-            class: 'btn btn-primary'
-        })
+        },
+        text: 'Add Person',
+        class: 'btn btn-primary'
+    })
 
-        var $deleteUser = '<button>x</button>'
-        $($deleteUser).on('click', 'th', function(){
-            console.log('asdas')
-        })
+    var $deleteUser = '<button id="deleteUser">x</button>'
+    $($deleteUser).on('click', 'th', function () {
+        console.log('asdas')
+    })
 
-        $('#addPerson').append($addPerson)
+    $('#addPerson').append($addPerson)
 
-        $.get(uri + 'users', function(resp){
-            const data = resp;
-            $.each(data, function(key, value){
-                $(tBody).append(
-                    '<tr><td>' + value.index + '</td><td>' + value.name + '</td> <td>' + value.gender + '</td><td>' + $deleteUser +'</td></tr>')
+    $.get(uri + 'users', function (resp) {
+        const data = resp;
+        $.each(data, function (key, value) {
+            $(tBody).append(
+                '<tr><td>' + value.index + '</td><td>' + value.name + '</td> <td>' + value.gender + '</td><td>' + $deleteUser + '</td></tr>')
                 .css('list-style-type', 'none')
-          })      
+        })
+        $('tbody').on('click', '#deleteUser', function (event) {
+            var elementOfTable = $('tbody #deleteUser').index(this)
+            var clickedObject = data[elementOfTable]
+            var id = clickedObject._id
+
+            $.ajax({
+                url: uri+'users?_id='+id,
+                type: 'DELETE',
+                result: function(resp){
+                    console.log(resp)
+                }
+            })
+                .then(function(){
+                    console.log('success')
+                })
+                .catch(function(err){
+                    console.log('error: '+err)
+                })
+
         })
     })
+})
