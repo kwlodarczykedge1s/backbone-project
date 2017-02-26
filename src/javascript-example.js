7cd77f73156e893b3043b95dc96c2a0443302547
Kacper Włodarczyk <kacper.wlodarczyk80@gmail.com>
2017-02-26 15:14:27 +0100

    javascript practice

--- src/javascript-example.js
+++ src/javascript-example.js
@@ -14,22 +14,21 @@
     }
 
     var clone = new Tree('Klon');
-    console.log('konstruktor: ' + clone.name)
+    console.log('konstruktor: ' + clone.name);
 
     //object seal
     var obj = {
         prop: function () { },
         foo: 'bar'
     };
+    obj.foo = 'baz';
+    obj.lumpy = 'woof';
+    delete obj.prop;
 
-    obj.foo = 'baz'
-    obj.lumpy = 'woof'
-    delete obj.prop
+    var o = Object.seal(obj);
 
-    var o = Object.seal(obj)
-
-    console.log('obj', obj)
-    console.log('o', o)
+    console.log('obj', obj);
+    console.log('o', o);
 
     // Object Values
     var obj1 = {
@@ -37,7 +36,7 @@
         foo: 'bar'
     };
 
-    console.log('Wartości obiektu:', Object.values(obj1))
+    console.log('Wartości obiektu:', Object.values(obj1));
 
     //Object.keys
 
@@ -48,35 +47,90 @@
 
     //Prototypy
 
-    console.log(Object.keys(obj2))
+    console.log(Object.keys(obj2));
 
-    Object.dupa = function(){
+    Object.dupa = function () {
         return 'dupa';
     }
 
     var obj4 = new Object();
-    console.log(obj4)
+    console.log(obj4);
 
-    //Proto
-    var Person = function(firstName, lastName, age){
-       this.firstName = firstName;
-       this.lastName = lastName;
-       this.age = age;
-
-       this.getFullName = function(){
-           return 'Witaj ' + this.firstName + ' ' + this.lastName;
-       }
-
-       this.getAge = function(){
-           return 'Twój wiek: ' + this.age;
-       }
+    //Klasy
+    var Person = function (firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
     }
-    
+
     var person1 = new Person("Marek", "Kowalski", 46);
     var person2 = new Person("Adrian", "Wołowski", 28)
-    console.log(person1.getFullName()+ ' / ' + person1.getAge())
-    console.log(person2.getFullName()+ ' / ' + person2.getAge())
 
+    //Korzystanie z prototypu
 
+    Person.prototype.getFullName = function () {
+        return 'Witaj ' + this.firstName + ' ' + this.lastName;
+    }
 
+    Person.prototype.getAge = this.getAge = function () {
+        return 'Twój wiek: ' + this.age;
+    }
+
+    console.log(person1.getFullName() + ' / ' + person1.getAge());
+    console.log(person2.getFullName() + ' / ' + person2.getAge());
+
+    //nadpisywanie metod
+    person1.getFullName = function () {
+        return 'Witaj ' + this.firstName.toUpperCase() + ' ' + this.lastName.toUpperCase();
+    }
+    console.log(person1.getFullName())
+
+    //Dziedziczenie wielokrotne
+
+    function Shape(sideLenghts) {
+        this._name = null;
+        this._sideLenghts = sideLenghts;
+    }
+
+    Shape.prototype.getPerimeter = function () {
+        this._sideLenghts.reduce(function (prevValue, value) {
+            return prevValue + value;
+        })
+    }
+    var shape1 = new Shape([20, 30, 40, 50]);
+
+    function Rectangle(sideLenghts) {
+        this._name = 'Prostokąt';
+        Shape.call(this, [sideLenghts[0], sideLenghts[1], sideLenghts[0], sideLenghts[1]])
+    }
+
+    var shape2 = new Rectangle([20, 10])
+    console.log(shape2); //Rectangle {_name: null, _sideLenghts: Array[4]}
+
+    function Square(sideLenght) {
+        Rectangle.call(this, [sideLenght, sideLenght]);
+        this._name = 'kwadrat';
+    }
+
+    var shape3 = new Square(20);
+    console.log(shape3); //Square {_name: "kwadrat", _sideLenghts: Array[4]}
+
+    // call()
+    var animal = {
+        race: 'Maltańczyk',
+        color: 'Czarny'
+    }
+
+    function dog() {
+        console.log('rasa: ' + this.race + ' / kolor: ' + this.color)
+    }
+
+    console.log(dog.call(animal)) // rasa: Maltańczyk / kolor: Czarny
+
+    //apply()
+    function sum(first, second, third){
+        return first + second + third;
+    }
+
+    sum.apply(this, [2,6,8]) // pierwszy argument this, drugi tablica
 })();
